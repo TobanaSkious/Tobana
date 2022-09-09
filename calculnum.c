a80c7a8bac0baf50e59e9418dffd7546ad36b586
TobanaSkious <112955831+TobanaSkious@users.noreply.github.com>
2022-09-09 17:10:52 +0100

    Update calculnum.c

--- calculnum.c
+++ calculnum.c
@@ -27,39 +27,8 @@ int main()
         prdt=nmbr1*nmbr2;
         printf("%f/%f=%f",nmbr1,nmbr2,prdt);
         break;
-    case'#include<stdio.h>
-#include<math.h>
-int main()
-{
-    float nmbr1 , nmbr2, somme , soustr , divis , prdt ;
-    char operation;
-    printf("donner la valeur du nombre 1 : \n");
-    scanf("%f",& nmbr1);
-    printf("donner la valeur du Nombre 2: \n");
-    scanf("%f",& nmbr2);
-    printf("choisiszze votre operation : \n");
-    scanf(" %c",& operation);
-    switch (operation){
-    case '+':
-        somme=nmbr1+nmbr2;
-        printf("%f+%f=%f",nmbr1,nmbr2,somme);
-          break;
-    case '-':
-        soustr=nmbr1-nmbr2;
-        printf("%f-%f=%f",nmbr1,nmbr2,soustr);
-        break;
-    case'/':
-        divis=nmbr1/nmbr2;
-        printf("%f/%f=%f",nmbr1,nmbr2,divis);
-        break;
-    case'*':
-        prdt=nmbr1*nmbr2;
-        printf("%f/%f=%f",nmbr1,nmbr2,prdt);
-        break;
-      
-    }
-return 0;
-}
+
+
     }
 return 0;
 }
